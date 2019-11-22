158d14d6513d38036e5b7a95a5573b3108ec941d
laxmi mali <57988612+mali375@users.noreply.github.com>
2019-11-22 12:49:09 +0530

    Add files via upload

---
+++ evenoddex1.py
@@ -0,0 +1,15 @@
+n=int(input("enetr elements:"))
+
+l= list(map(int,input().strip().split(" ")))
+
+odd=[]
+even=[]
+
+for i in l:
+    if(i%2!=0):
+        odd.append(i)
+    else:
+        even.append(i)
+
+print("list of odd number is:",odd)
+print("list of even number is:",even)
