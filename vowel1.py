dc2e83e859ee9b1b078b84030115ccb9e4f5c1c2
laxmi mali <57988612+mali375@users.noreply.github.com>
2019-11-22 12:49:51 +0530

    Add files via upload
    vowel

---
+++ vowel1.py
@@ -0,0 +1,11 @@
+first=input("Enter string:\n")
+second=""
+vowels=['a','e','i','o','u']
+for i in first:
+    if i in vowels:
+        #firts=first+i.upper()
+        second=second+i.upper()
+    else:
+            second=second+i
+            #print(second)                           
+     
